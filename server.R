6079c8ae297e3d839bf41c04fd44296ad283d0e0
Sebastian Pino <spinos@unal.edu.co>
2018-03-30 23:38:28 -0500

    Adición del mapa de calor

--- server.R
+++ server.R
@@ -6,18 +6,38 @@
 #
 
 library(shiny)
+library(rgdal)
+library(leaflet)
+library(leaflet.extras)
+library(raster)
+library(colorRamps)
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  departamentos <- shapefile("Departamentos/departamentos.shp", encoding="UTF-8", use_iconv = TRUE)
+  
+  output$map <- renderLeaflet({
+    
+    # Generación de números aleatorios, suponiendo que son probabilidades
+    departamentos@data$prob <- runif(33, min=0, max=100)
+    
+    #Creacion de los atributos del mapa
+    pal <-colorNumeric(palette=blue2red(100),domain=c(0,100))
+    popup<-paste(departamentos@data$DEPTO,paste("Probabilidad ",departamentos@data$prob," %"),sep="<br/>")
+    
+    #Definicion del mapa
+    mapaDepartamentos <- leaflet(departamentos)
+    mapaDepartamentos <- addProviderTiles(mapaDepartamentos, provider="OpenStreetMap.Mapnik")
+    mapaDepartamentos <- addPolygons(mapaDepartamentos,
+                                     popup = popup,
+                                     color=pal(as.integer(departamentos@data$prob)),
+                                     opacity = 0.5,
+                                     fillOpacity = 0.5,
+                                     weight = 1)
+    mapaDepartamentos <-addLegend(mapaDepartamentos,"topright",pal=pal,values=departamentos@data$prob, 
+                                  title="Probabilidad",
+                                  opacity = 1)
+    mapaDepartamentos
+    
   })
-
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,41 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(leaflet)
 
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
+shinyUI(div(
+  tags$head(#Se incluye algunos estilos que usa la app
+    includeCSS("styles.css")),
+  navbarPage(
+    "App-Familias",
+    
+    #Mapa: Tab para visualizar el mapa correspondiente
+    #a la probabilidad dada por el modelo para cada uno
+    #de los departamentos de Colombia (De acuerdo a los
+    # paramentros de filtrado).
+    
+    tabPanel(title = "Mapa",
+             div(
+               class = "outer",
+               
+               #map: Mapa de calor por departamento en Colombia
+               leafletOutput("map", width = "100%", height = "100%"),
+               
+               absolutePanel(
+                 id = "controls",
+                 class = "panel panel-default",
+                 fixed = TRUE,
+                 draggable = TRUE,
+                 top = "70%",
+                 left = "auto",
+                 right = 20,
+                 bottom = "auto",
+                 width = 330,
+                 height = "auto",
+                 h3("Controles", class = "text-center"),
+                 selectInput(inputId="year", label="Año", choices= c("2015", "2016", "2017")),
+                 selectInput(inputId="year", label="Año", choices= c("2015", "2016", "2017"))
+               )
+             )),
+    tabPanel(title = "Analisis Descriptivo"),
+    tabPanel(title = "Acerca de")
   )
 ))
