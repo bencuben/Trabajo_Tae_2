2499cc7c11ee580bfd27d6fe7783c5bb7f36bffb
Daniel Cano <dacanoc@unal.edu.co>
2018-04-05 23:21:53 -0500

    Primer acercamiento al modelo por departamento

--- server.R
+++ server.R
@@ -11,33 +11,107 @@ library(leaflet)
 library(leaflet.extras)
 library(raster)
 library(colorRamps)
+require(foreign)
+require(nnet)
+require(ggplot2)
+require(reshape2)
+library (MASS)
 
 shinyServer(function(input, output) {
   
-  departamentos <- shapefile("Departamentos/departamentos.shp", encoding="UTF-8", use_iconv = TRUE)
+  withProgress(message="Cargando datos", {
+    departamentos <- shapefile("Departamentos/departamentos.shp", encoding="UTF-8", use_iconv = TRUE)
+    incProgress(25)
+    departamentos@data$DPTO_CCDGO <- as.integer(departamentos@data$DPTO_CCDGO)
+    incProgress(25)
+    departamentos <- departamentos[order(departamentos@data$DPTO_CCDGO),]
+    incProgress(25)
+    datos <- read.csv("BDs/Caracteristicas y composicion del hogar.csv")
+    incProgress(25)
+  })
   
-  output$map <- renderLeaflet({
+  withProgress(message="Creando modelo", {
+    #### Eliminando los valñores ausentes
     
+    machetazo<- na.omit(datos)
+    
+    #### Eliminamos las columnas debido al alto número de valores "infinitos"
+    
+    machetazo<- machetazo[,-c(21,24,27)]
+    
+    
+    #### Eliminamos todos los valores infinitos con la función is.finite y dejamos finalmente una basede datos limpia para trabajar
+    
+    guadanazo<-machetazo[which(is.finite(machetazo$P6071)
+                               & is.finite(machetazo$P6071S1)
+                               & is.finite(machetazo$P6088)
+                               &  is.finite(machetazo$P1903)
+                               & is.finite(machetazo$P6087)),  ]
+      #### Procedemos a eliminar todas las variables que consideramos bno singinificativas o aportantes para este caso
+    incProgress(25)
+    
+    
+    #### recodificamos las variables para que sean de tipo categorico
+    guadanazo<- guadanazo[,-c(1,2,3,4,5,6)]
+    daticos<-guadanazo
+    
+    for(i in 1:28){
+      daticos[,i]<- as.factor(guadanazo[,i])
+    }
+    incProgress(25)
+    ### Todos los nombres
+    
+    
+    
+    daticos$P6040<- as.integer(daticos$P6040)
+    daticos$P767<- as.integer(daticos$P767)
+    daticos<-daticos[,-11]
+    daticos<-daticos[,-c(5,7,9)]
+    incProgress(25)
+    daticos$P6076S1<-as.factor(daticos$P6076S1)
+    
+    #modelo luego del step aic
+    modelo.fullhd<-multinom(formula = P6076S1 ~ P6077 + P6080 +  + P6096 + 
+                              P767 + P6081 + P5502, data = daticos)
+    incProgress(25)
+  })
+  
+  output$mapDep <- renderLeaflet({
+    
+    vP6077 <- gsub("([0-9]+).*$", "\\1", input$P6077)
+    vP6080 <- gsub("([0-9]+).*$", "\\1", input$P6080)
+    vP6096 <- gsub("([0-9]+).*$", "\\1", input$P6096)
+    vP767 <- as.integer(gsub("([0-9]+).*$", "\\1", input$P767))
+    vP6081 <- gsub("([0-9]+).*$", "\\1", input$P6081)
+    vP5502 <- gsub("([0-9]+).*$", "\\1", input$P5502)
+    
+    sujeto<-data.frame(P6077= vP6077,P6080=vP6080,P6096=vP6096,P767=vP767,P6081=vP6081,P5502=vP5502)
+    probabilidades <- predict(modelo.fullhd,sujeto,type="prob")
+    
+    # Guanía no está en la base de datos original, probabilidad siempre será 0%
+    probabilidades[30] <- 0
+    
+    departamentos@data$prob <- probabilidades
     # Generación de números aleatorios, suponiendo que son probabilidades
-    departamentos@data$prob <- runif(33, min=0, max=100)
+    # departamentos@data$prob <- runif(33, min=0, max=100)
     
     #Creacion de los atributos del mapa
-    pal <-colorNumeric(palette=blue2red(100),domain=c(0,100))
-    popup<-paste(departamentos@data$DEPTO,paste("Probabilidad ",departamentos@data$prob," %"),sep="<br/>")
+    pal <-colorNumeric(palette=blue2red(max(departamentos@data$prob*100)),domain=c(0,departamentos@data$prob*100))
+    popup<-paste(departamentos@data$DEPTO,paste("Probabilidad ", round(departamentos@data$prob*100,digits=2)," %"),sep="<br/>")
     
     #Definicion del mapa
     mapaDepartamentos <- leaflet(departamentos)
     mapaDepartamentos <- addProviderTiles(mapaDepartamentos, provider="OpenStreetMap.Mapnik")
     mapaDepartamentos <- addPolygons(mapaDepartamentos,
                                      popup = popup,
-                                     color=pal(as.integer(departamentos@data$prob)),
+                                     color=pal(as.integer(departamentos@data$prob*100)),
                                      opacity = 0.5,
                                      fillOpacity = 0.5,
                                      weight = 1)
-    mapaDepartamentos <-addLegend(mapaDepartamentos,"topright",pal=pal,values=departamentos@data$prob, 
+    mapaDepartamentos <-addLegend(mapaDepartamentos,"topright",pal=pal,values=departamentos@data$prob*100, 
                                   title="Probabilidad",
                                   opacity = 1)
-    mapaDepartamentos
+    mapaDepartamentos 
     
   })
 })
--- ui.R
+++ ui.R
@@ -12,30 +12,49 @@ shinyUI(div(
     #de los departamentos de Colombia (De acuerdo a los
     # paramentros de filtrado).
     
-    tabPanel(title = "Mapa",
+    tabPanel(title = "Por Departamento",
              div(
                class = "outer",
                
                #map: Mapa de calor por departamento en Colombia
-               leafletOutput("map", width = "100%", height = "100%"),
+               leafletOutput("mapDep", width = "100%", height = "100%"),
                
                absolutePanel(
                  id = "controls",
                  class = "panel panel-default",
                  fixed = TRUE,
                  draggable = TRUE,
-                 top = "70%",
-                 left = "auto",
-                 right = 20,
+                 top = 40,
+                 left = 80,
+                 right = "auto",
                  bottom = "auto",
                  width = 330,
                  height = "auto",
                  h3("Controles", class = "text-center"),
-                 selectInput(inputId="year", label="Año", choices= c("2015", "2016", "2017")),
-                 selectInput(inputId="year", label="Año", choices= c("2015", "2016", "2017"))
+                 selectInput(inputId="P6077", label="Lugar donde habitaba anteriormente", 
+                             choices= c("1 - El centro urbano donde está la alcaldía", 
+                                        "2 - Un corregimiento, inspección de policía, caserío, vereda o campo")),
+                 selectInput(inputId="P6080", label="De acuerdo con su cultura,pueblo o rasgos físicos, es o se reconoce comó",
+                             choices= c("1 - Indígena","2 - Gitano","3 - Raizal del archipiélago","4 - Palenquero", 
+                                        "5 - Negro, mulato o afrodescendiente","6 - Ninguno de los anteriores")),
+                 selectInput(inputId="P6096", label="Cuál fue la razón principal para cambiar la residencia al municipio actual?",
+                             choices= c("1 - Dificultad para encontrar trabajo o ausencia de medios de subsistencia", 
+                                        "2 - Riesgo o consecuencia de desastre natural", 
+                                        "3 - Amenaza o riesgo para su vida, su libertad o su integridad física, ocasionada por la violencia",
+                                        "4 - Necesidad de educación", "5 - Porque se casó o formó pareja", "6 - Motivos de salud",
+                                        "7 - Mejorar la vivienda o localización", "8 - Mejores  oportunidades laborales o de  negocio",
+                                        "9 - Acompañar a otro(s) miembro(s) del hogar","10 - Otra")),
+                 numericInput(inputId="P767", label="Cuántos años continuos hace que vive aquí en este municipio?", min = 1 , value=1),
+                 selectInput(inputId="P6081", label="El padre vive en este hogar?",
+                             choices=c("1 - Sí","2 - No", "3 - Fallecido"), selected="2 - No"),
+                 selectInput(inputId="P5502", label="Situación sentimental actual", 
+                             choices=c("1 - No está casado(a) y vive en pareja hace menos de dos años", 
+                                      "2 - No está casado(a) y vive en pareja hace dos años o más", "3 - Está viudo(a)", 
+                                      "4 - Está separado(a) o divorciado(a)", "5 - Está soltero(a)", "6 - Está casado(a)"))
+                 
                )
              )),
-    tabPanel(title = "Analisis Descriptivo"),
+    tabPanel(title = "Por región"),
     tabPanel(title = "Acerca de")
   )
 ))
