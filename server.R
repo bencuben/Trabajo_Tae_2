17f842f4932f3e763aa69890aaf1c7d2a2d247ed
Daniel Cano <dacanoc@unal.edu.co>
2018-04-07 12:55:38 -0500

    Corregida asignación probabilidades a regiones e identación de código

--- server.R
+++ server.R
@@ -36,30 +36,30 @@ shinyServer(function(input, output) {
     ################################################################################## Modelo por departamento
     #### Eliminando los valñores ausentes
     
-    machetazo<- na.omit(datos)
+    omit<- na.omit(datos)
     
     #### Eliminamos las columnas debido al alto número de valores "infinitos"
     
-    machetazo<- machetazo[,-c(21,24,27)]
+    omit<- omit[,-c(21,24,27)]
     
     
     #### Eliminamos todos los valores infinitos con la función is.finite y dejamos finalmente una basede datos limpia para trabajar
     
-    guadanazo<-machetazo[which(is.finite(machetazo$P6071)
-                               & is.finite(machetazo$P6071S1)
-                               & is.finite(machetazo$P6088)
-                               &  is.finite(machetazo$P1903)
-                               & is.finite(machetazo$P6087)),  ]
+    omitinfinite<-omit[which(is.finite(omit$P6071)
+                               & is.finite(omit$P6071S1)
+                               & is.finite(omit$P6088)
+                               &  is.finite(omit$P1903)
+                               & is.finite(omit$P6087)),  ]
     #### Procedemos a eliminar todas las variables que consideramos bno singinificativas o aportantes para este caso
     incProgress(25)
     
     
     #### recodificamos las variables para que sean de tipo categorico
-    guadanazo<- guadanazo[,-c(1,2,3,4,5,6)]
-    daticos<-guadanazo
+    omitinfinite<- omitinfinite[,-c(1,2,3,4,5,6)]
+    daticos<-omitinfinite
     
     for(i in 1:28){
-      daticos[,i]<- as.factor(guadanazo[,i])
+      daticos[,i]<- as.factor(omitinfinite[,i])
     }
     incProgress(25)
     ### Todos los nombres
@@ -74,7 +74,7 @@ shinyServer(function(input, output) {
     daticos$P6076S1<-as.factor(daticos$P6076S1)
     
     #modelo luego del step aic
-    modelo.fullhd<-multinom(formula = P6076S1 ~ P6077 + P6080 +  + P6096 + 
+    model1<-multinom(formula = P6076S1 ~ P6077 + P6080 +  + P6096 + 
                               P767 + P6081 + P5502, data = daticos)
     incProgress(25)
     
@@ -123,7 +123,7 @@ shinyServer(function(input, output) {
     
     #########
     
-    mod.imaginario<-multinom(formula = var2.res ~ P6080 + P4567 + P8520S3+
+    model2<-multinom(formula = var2.res ~ P6080 + P4567 + P8520S3+
                                P8520S1A1 + CLASE + P767 + P6081 + P5661S4 + P6077 + P1070, 
                              data = datos2)
     incProgress(25)
@@ -139,7 +139,7 @@ shinyServer(function(input, output) {
     vP5502 <- gsub("([0-9]+).*$", "\\1", input$P5502)
     
     sujeto<-data.frame(P6077= vP6077,P6080=vP6080,P6096=vP6096,P767=vP767,P6081=vP6081,P5502=vP5502)
-    probabilidades <- predict(modelo.fullhd,sujeto,type="prob")
+    probabilidades <- predict(model1,sujeto,type="prob")
     
     # Guanía no está en la base de datos original, probabilidad siempre será 0%
     probabilidades[30] <- 0
@@ -183,15 +183,15 @@ shinyServer(function(input, output) {
     sujeto2<-data.frame(P6080=vP6080_2, P4567=vP4567_2, P8520S3 = vP8520S3_2, P8520S1A1 = vP8520S1A1_2, CLASE = vCLASE_2, 
                         P767=vP767_2, P6081 = vP6081_2, P5661S4= vP5661S4_2, P6077 = vP6077_2, P1070= vP1070_2)
     
-    probs <- as.vector(predict(mod.imaginario,sujeto2,type="prob"))
-    
+    probs <- as.vector(predict(model2,sujeto2,type="prob"))
+
     departamentos@data$prob2 <- seq(0, 0, length=33)
     
-    departamentos@data[departamentos@data$region == "ANDINA",]$prob2 <- probs[1]
-    departamentos@data[departamentos@data$region == "CARIBE",]$prob2 <- probs[2]
-    departamentos@data[departamentos@data$region == "PACIFICA",]$prob2 <- probs[3]
+    departamentos@data[departamentos@data$region == "ANDINA",]$prob2 <- probs[2]
+    departamentos@data[departamentos@data$region == "CARIBE",]$prob2 <- probs[3]
+    departamentos@data[departamentos@data$region == "PACIFICA",]$prob2 <- probs[5]
     departamentos@data[departamentos@data$region == "ORINOQUIA",]$prob2 <- probs[4]
-    departamentos@data[departamentos@data$region == "AMAZONIA",]$prob2 <- probs[5]
+    departamentos@data[departamentos@data$region == "AMAZONIA",]$prob2 <- probs[1]
     
     #Creacion de los atributos del mapa
     pal <-colorNumeric(palette=blue2red(max(departamentos@data$prob2*100)),domain=c(0,departamentos@data$prob2*100))
